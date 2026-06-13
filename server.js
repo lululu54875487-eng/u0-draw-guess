ad8febc0577ab94895fcfb28e1e598a8bdbec687
lululu54875487-eng <lululu54875487@gmail.com>
2026-06-13 14:42:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*"
-  }
+  origin: "https://u0-draw-guess-v2.onrender.com",
+  methods: ["GET", "POST"]
+      }
 });
 
 app.use(express.static(path.join(__dirname, "public")));
